503768aa47a5b78c1693436429f49a80c5490034
Sonawane Vaibhaw Madukar <93070004+VaibhawSonawane1@users.noreply.github.com>
2023-08-06 01:25:29 +0530

    Time: 12 ms (51.03%), Space: 8.8 MB (65.80%) - LeetHub

---
+++ 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string removeTrailingZeros(string num) {
+       int i=num.length()-1;
+        while(num[i]=='0')
+        {
+            num.pop_back();
+            i--;
+        }
+        return num;
+
+    }
+};
